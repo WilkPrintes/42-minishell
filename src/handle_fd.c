0bc7b7c335960e68bb63f5b5d0b37c5a02c68438
Lucas Gomes <lucferna@student.42sp.org.br>
2022-07-22 21:49:36 +0200

    fixing echo

--- src/handle_fd.c
+++ src/handle_fd.c
@@ -6,7 +6,7 @@
 /*   By: lucferna <lucferna@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 21:06:02 by lucferna          #+#    #+#             */
-/*   Updated: 2022/07/20 19:24:20 by lucferna         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:19:58 by lucferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,19 @@ void	reset_original_fd(int *original_fd, int *dif_fd)
 		unlink(".temp_file");
 	dup2(original_fd[0], STDIN_FILENO);
 	dup2(original_fd[1], STDOUT_FILENO);
+}
+
+int	biggest(char *s1, char *s2)
+{
+	int	len1;
+	int	len2;
+	int	size;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (len1 > len2)
+		size = len1;
+	else
+		size = len2;
+	return (size);
 }
